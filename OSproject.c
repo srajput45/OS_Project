b3ca7401116ea271560465403b85238a5f9414d5
srajput45 <43792808+srajput45@users.noreply.github.com>
2019-04-06 19:18:18 +0530

    Update OSproject.c
    I have added a queue3() function. In which first come first serve algo. runs.

--- OSproject.c
+++ OSproject.c
@@ -54,5 +54,47 @@ main()
             queue3();
         }
 
+        void queue3()
+    {
+        printf("\nFirst come First Serve\n");
+
+        int n,bt[20],wt[20],tt[20],avg_wt=0,avg_tt=0,i,j;
+
+        printf("Enter total number of processes(maximum 20):");
+        scanf("%d",&n);
+
+        printf("\nEnter Process Burst Time\n");
+        for(i=0;i<n;i++)
+        {
+            printf("P[%d]:",i+1);
+            scanf("%d",&bt[i]);
+        }
+
+        wt[0]=0;
+
+        for(i=1;i<n;i++)
+        {
+            wt[i]=0;
+            for(j=0;j<i;j++)
+                wt[i]+=bt[j];
+        }
+
+        printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
+
+        for(i=0;i<n;i++)
+        {
+            tt[i]=bt[i]+wt[i];
+            avg_wt+=wt[i];
+            avg_tt+=tt[i];
+            printf("\nP[%d]\t\t%d\t\t%d\t\t%d",i+1,bt[i],wt[i],tt[i]);
+        }
+
+        avg_wt/=i;
+        printf("\n\nAverage Waiting Time:%d",avg_wt);
+
+        avg_tt/=i;
+        printf("\nAverage Turnaround Time:%d",avg_tt);
+    }
+
 
 }
