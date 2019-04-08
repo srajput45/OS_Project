019472c97a017b9e419bf4523d959ec5c93894c3
srajput45 <43792808+srajput45@users.noreply.github.com>
2019-04-09 00:12:23 +0530

    added queue2()
    queue2() this function performs the priority sheduling

--- OSproject.c
+++ OSproject.c
@@ -149,6 +149,76 @@ main()
             printf("%f",(float)g/(float)n)
          }
 
+         void Queue2()
+    {
+        printf("\nPriority Scheduling\n");
+        int i,j,n,pos,temp,avg_wt,avg_tt;
+        int total=0,bt[20],p[20],wt[20],tt[20],pr[20];
+
+        printf("Enter Total Number of Process:");
+        scanf("%d",&n);
+
+        printf("\nEnter Burst Time and Priority\n");
+        for(i=0;i<n;i++)
+        {
+            printf("\nP[%d]\n",i+1);
+            printf("Burst Time:");
+            scanf("%d",&bt[i]);
+            printf("Priority:");
+            scanf("%d",&pr[i]);
+            p[i]=i+1;
+        }
+
+        for(i=0;i<n;i++)
+        {
+            pos=i;
+            for(j=i+1;j<n;j++)
+            {
+                if(pr[j]<pr[pos])
+                    pos=j;
+            }
+
+            temp=pr[i];
+            pr[i]=pr[pos];
+            pr[pos]=temp;
+
+            temp=bt[i];
+            bt[i]=bt[pos];
+            bt[pos]=temp;
+
+            temp=p[i];
+            p[i]=p[pos];
+            p[pos]=temp;
+        }
+
+        wt[0]=0;
+
+
+        for(i=1;i<n;i++)
+        {
+            wt[i]=0;
+            for(j=0;j<i;j++)
+                wt[i]+=bt[j];
+
+            total+=wt[i];
+        }
+
+        avg_wt=total/n;
+        total=0;
+
+        printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+        for(i=0;i<n;i++)
+        {
+            tt[i]=bt[i]+wt[i];
+            total+=tt[i];
+            printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tt[i]);
+        }
+
+        avg_tt=total/n;
+        printf("\n\nAverage Waiting Time=%d",avg_wt);
+        printf("\nAverage Turnaround Time=%d\n",avg_tt);
+    }
+
         void queue3()
     {
         printf("\nFirst come First Serve\n");
